b98c28d00b71d77e4b5a9080e89a90a5c9f04536
ilyes rafai <rafaiilyes3@gmail.com>
2026-06-10 18:49:01 +0100

    DHAD Hackers landing pages

--- src/app/sections/Fab.jsx
+++ src/app/sections/Fab.jsx
@@ -16,82 +16,50 @@ export default function Fab() {
                         {/* <div className="h-0.5 bg-linear-to-r from-transparent via-[#FF0000] to-transparent"></div> */}
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 text-center pt-8 gap-16 md:gap-8">
                                 <div className="text-balance">
-                                        {/* <Image
+                                        <Image
                                                 src="/img/learn/stacks.png"
                                                 alt="On-Site Learning"
                                                 width={200}
                                                 height={200}
                                                 loading="lazy"
                                                 className="mx-auto mb-4"
-                                        /> */}
-                                        <video width="320" height="240" muted loop preload="none" autoPlay>
-                                                <source src="/videos/knowledge4.mp4" type="video/mp4" />
-                                                <track
-                                                        src="/path/to/captions.vtt"
-                                                        kind="subtitles"
-                                                        srcLang="en"
-                                                        label="English"
-                                                />
-                                                Your browser does not support the video tag.
-                                        </video>
+                                        />
                                         <h4 className="text-xl font-semibold mb-4">{t("title_1")}</h4>
                                         <Text>{t("description1")}</Text>
                                 </div>
                                 <div className="text-balance">
-                                        {/* <Image
+                                        <Image
                                                 src="/img/learn/time.png"
                                                 alt="On-Site Learning"
                                                 width={200}
                                                 height={200}
                                                 loading="lazy"
                                                 className="mx-auto mb-4"
-                                        /> */}
-                                        <video width="320" height="240" muted loop preload="none" autoPlay>
-                                                <source src="/videos/knowledge2.mp4" type="video/mp4" />
-                                                <track
-                                                        src="/path/to/captions.vtt"
-                                                        kind="subtitles"
-                                                        srcLang="en"
-                                                        label="English"
-                                                />
-                                                Your browser does not support the video tag.
-                                        </video>
+                                        />
                                         <h4 className="text-xl font-semibold mb-4">{t("title_2")}</h4>
                                         <Text>{t("description2")}</Text>
                                 </div>
                                 <div className="text-balance">
-                                        {/* <Image
+                                        <Image
                                                 src="/img/learn/platforme.png"
                                                 alt="On-Site Learning"
                                                 width={200}
                                                 height={200}
                                                 loading="lazy"
                                                 className="mx-auto mb-4"
-                                        /> */}
-                                        <video width="320" height="240" muted loop preload="none" autoPlay>
-                                                <source src="/videos/knowledge3.mp4" type="video/mp4" />
-                                                <track
-                                                        src="/path/to/captions.vtt"
-                                                        kind="subtitles"
-                                                        srcLang="en"
-                                                        label="English"
-                                                />
-                                                Your browser does not support the video tag.
-                                        </video>
+                                        />
                                         <h4 className="text-xl font-semibold mb-4">{t("title_3")}</h4>
                                         <Text>{t("description3")}</Text>
                                 </div>
                                 <div className="text-balance">
-                                        <video width="320" height="240" muted loop preload="none" autoPlay>
-                                                <source src="/videos/knowledge.mp4" type="video/mp4" />
-                                                <track
-                                                        src="/path/to/captions.vtt"
-                                                        kind="subtitles"
-                                                        srcLang="en"
-                                                        label="English"
-                                                />
-                                                Your browser does not support the video tag.
-                                        </video>
+                                        <Image
+                                                src="/img/learn/knowledge.png"
+                                                alt="On-Site Learning"
+                                                width={200}
+                                                height={200}
+                                                loading="lazy"
+                                                className="mx-auto mb-4"
+                                        />
                                         <h4 className="text-xl font-semibold mb-4">{t("title_4")}</h4>
                                         <Text>{t("description4")}</Text>
                                 </div>
